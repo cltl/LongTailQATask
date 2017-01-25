90c353d06a00d72ce22ea8e76697989b98ac616f
filipdbrsk <filip.dbrsk@gmail.com>
2017-01-25 15:15:17 +0100

    Ready to process documents

---
+++ utils.py
@@ -0,0 +1,50 @@
+import urllib.parse
+import requests
+import os
+import shutil
+
+API="https://api.diffbot.com/v3/article"
+TOKEN="1f5fcfe62127906ba56274d11c019ac8"
+CORPUSDIR="the_violent_corpus/"
+
+def hash_uri(u):
+	import hashlib
+	hash_obj=hashlib.md5(u.encode())
+	return hash_obj.hexdigest()
+
+# Iterate sources and obtain text
+def whats_behind_a_uri(my_uri):
+	q = {'url': my_uri, 'token': TOKEN, 'paging': 'false'}
+	req_url = API + '?' + urllib.parse.urlencode(q)
+	r = requests.get(url=req_url)
+	try:
+		page = r.json()
+		if 'objects' in page:
+			my_object=page['objects'][0]
+			if 'estimatedDate' in my_object:
+				dct=my_object['estimatedDate']
+			else:
+				dct='NODATE'
+				
+			article= {'uri': my_uri, 'title': my_object['title'], 'dct': dct, 'content': my_object['text'], 'id': hash_uri(my_uri)}
+		else:
+			article=None
+	except ValueError:
+		print("Error with the article %s" % my_uri)
+		article=None
+	return article
+
+def reset_dir(index):
+	directory="%s%s/" % (CORPUSDIR, str(index))
+	if os.path.exists(directory):
+		shutil.rmtree(directory)
+	os.makedirs(directory)
+	return directory
+
+def dump_to_file(article, targetdir):
+#	try:
+	with open("%s%s" % (targetdir, article['id']), 'w') as w:
+		w.write("%s\n%s\n%s" % (article['title'], article['dct'], article['content']))
+	print("Article %s written!" % article['uri'])
+#	except:
+#		print("Error when writting to article")
