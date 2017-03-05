25f4fd02b59e241e387787bbba5dd1a48f06922b
MartenPostma <martenp@gmail.com>
2017-03-05 00:59:10 +0000

    Updated the extraction script that uses archive.org and newspaper

--- EventRegistries/GunViolence/CheckArchiveOrg.py
+++ EventRegistries/GunViolence/CheckArchiveOrg.py
@@ -5,6 +5,7 @@
 
 import pickle
 import pandas
+from multiprocessing.dummy import Pool as ThreadPool
 from collections import Counter, defaultdict
 import operator
 import utils
@@ -14,44 +15,11 @@ import requests
 from urllib.parse import urlencode, quote_plus
 import time
 
+cooldown_time=0.05
 
 # In[2]:
 
 import classes
-
-
-# In[3]:
-
-urls_and_paths = [('frames/children_killed', 'http://www.gunviolencearchive.org/children-killed'),
-                  ('frames/children_injured', 'http://www.gunviolencearchive.org/children-injured'),
-                  ('frames/teens_killed', 'http://www.gunviolencearchive.org/teens-killed'),
-                  ('frames/teens_injured', 'http://www.gunviolencearchive.org/teens-injured'),
-                  ('frames/accidental_deaths', 'http://www.gunviolencearchive.org/accidental-deaths'),
-                  ('frames/accidental_injuries', 'http://www.gunviolencearchive.org/accidental-injuries'),
-                  ('frames/accidental_deaths_children', 'http://www.gunviolencearchive.org/accidental-child-deaths'),
-                  ('frames/accidental_injuries_children', 'http://www.gunviolencearchive.org/accidental-child-injuries'),
-                  ('frames/accidental_deaths_teens', 'http://www.gunviolencearchive.org/accidental-teen-deaths'),
-                  ('frames/accidental_injuries_teens', 'http://www.gunviolencearchive.org/accidental-teen-injuries'),
-                  ('frames/officer_involved_shootings', 'http://www.gunviolencearchive.org/officer-involved-shootings'),
-                  ('frames/mass_shootings_2013', 'http://www.gunviolencearchive.org/reports/mass-shootings/2013'),
-                  ('frames/mass_shootings_2014', 'http://www.gunviolencearchive.org/reports/mass-shootings/2014'),
-                  ('frames/mass_shootings_2015', 'http://www.gunviolencearchive.org/reports/mass-shootings/2015'),
-                  ('frames/mass_shootings', 'http://www.gunviolencearchive.org/mass-shooting')]
-CORPUS_NAME = 'the_violent_corpus'
-
-
-# In[4]:
-
-frames = []
-for df_path, url in urls_and_paths:
-    with open(df_path, 'rb') as infile:
-        df = pickle.load(infile)
-        frames.append(df)
-df = pandas.concat(frames)
-len(df)
-
-
-# In[5]:
 
 def get_sources(dataframe):
     """
@@ -65,37 +33,87 @@ def get_sources(dataframe):
         sources.update(row['incident_sources'])
     return sources
 
-
 # In[6]:
 
 def generate_archive_uri(article_uri):
     archive_api='http://archive.org/wayback/available?'
     params={'url': article_uri}
     encoded_uri=archive_api + urlencode(params)
-    print(encoded_uri)
-    r=requests.get(encoded_uri)
-    j=r.json()
     try:
+        r=requests.get(encoded_uri)
+        j=r.json()
         closest=j['archived_snapshots']['closest']
         if all([closest['available'], closest['status']=='200']):
             return closest['url']
     except:
+        print("No archive version for %s" % article_uri)
         return ''
 
-archive_uri=generate_archive_uri('http://www.wdam.com/story/27682998/1-dead-4-injured-in-nightclub-shooting')
+def website_extraction(original_url, max_sec=5, debug=False):
+    """
+    attempt to obtain:
+    1. title
+    2. document
+    3. document creation time
+    
+    :param str url: url of news article
+    :param int max_sec: maximum number of seconds to wait for a response after downloading
+    :param bool debug: if debug, print info to stdout
+    
+    :rtype: dict
+    :return: dict containing keys 
+    1. 'title', 
+    2. 'document'
+    3. [most reliable] 'publish_date'
+    4. [slightly less reliable] 'published_time'
+    5. [slightly slightly less reliable] 'date'
+    """
+    if not original_url:
+        return classes.NewsItem(
+	    title='',
+	    content='',
+	    dct=None
+        )
+    global no_archive_version
+    global no_date_articles
+    global extraction_error
+    global all_good
 
-
-# In[7]:
-
-def download_and_parse_uri(url):
+    url=generate_archive_uri(original_url)
+    if not url:
+        no_archive_version+=1
+        return classes.NewsItem(
+                title='',
+                content='',
+                dct=None
+            )
     language='en'
     a=Article(url, language)
     a.download()
+    attempts = 0
+
+    while not a.is_downloaded:
+        time.sleep(1)
+        attempts += 1
+        
+        if attempts == max_sec:
+            extraction_error+=1
+            print("Extraction error with the article %s" % url)
+            return classes.NewsItem(
+                title='',
+                content='',
+                dct=None
+            )
+
     a.parse()
     title=a.title
     content=a.text
-    dct=a.publish_date or a.meta_data['date']
-
+    dct=a.publish_date or a.meta_data['date'] or a.meta_data['published_time']
+    if not dct:
+        print("No date found for article %s" % url)
+        no_date_articles+=1
+    else:
+        all_good+=1
     news_item=classes.NewsItem(
         title=title,
         content=content,
@@ -103,36 +121,57 @@ def download_and_parse_uri(url):
     )
     return news_item
 
-item=download_and_parse_uri(archive_uri)
+def article_iterable(sources, num_sources=None):
+    """
+    article generator
+    
+    :param pandas.core.frame.DataFrame df: gunviolence dataframe
+    :param int maximum: restrict to num_source, if None all will be returned
+    
+    :rtype: generator
+    :return: generator of news articles
+    """
+    counter = 0
+    for source in sources:
+                
+        if num_sources is None:
+            yield source
+        else:
+            if counter < num_sources:
+                yield source
+            else:
+                break
+        counter += 1
 
 
-# In[ ]:
+t1=time.time()
 
-cooldown_time=0.1
+frames = [
+          #'mass_shootings',
+          #'mass_shootings_2013',
+          #'mass_shootings_2014',
+          'mass_shootings_2015']
+df = pandas.concat([pandas.read_pickle('frames/' + frame)
+                    for frame in frames])
 
-sources=get_sources(df)
-has_archive_version=0
 no_archive_version=0
 no_date_articles=0
 extraction_error=0
+all_good=0
+
+sources=get_sources(df)
+"""
+iterable = article_iterable(sources, num_sources=100)
+pool = ThreadPool(1) 
+results = pool.map(website_extraction, iterable)
+"""
+
 for index, source in enumerate(sources):
     print("article no.", index)
     if source!='':
-        archive_uri=generate_archive_uri(source)
-        if archive_uri:
-            has_archive_version+=1
-            try: 
-                item=download_and_parse_uri(archive_uri)
-            except:
-                extraction_error+=1
-                continue
-            if not item.dct:
-                print("No date information found!")
-                no_date_articles+=1
-        else:
-            no_archive_version+=1
-            #print("No archive.org versions found!")
-
+        results=website_extraction(source)
         time.sleep(cooldown_time)
 
-print(has_archive_version, no_archive_version, no_date_articles, extraction_error)
+t2=time.time()
+print("Time elapsed", t2-t1)
+print(all_good, no_archive_version, no_date_articles, extraction_error)
