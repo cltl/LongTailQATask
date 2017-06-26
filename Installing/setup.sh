dcb58331d36c9a77d77df324a0d852a3500480b5
MartenPostma <martenp@gmail.com>
2017-06-26 09:54:58 +0200

    installing

--- Installing/setup.sh
+++ Installing/setup.sh
@@ -6,9 +6,6 @@ echo "If there is any error in one of these steps, the script will exit."
 #variable names
 cwd=${PWD#*}
 
-vir_env_dir='long_tail_venv'
-ext_modules='external_modules.txt'
-
 function command_check () {
 
 RETVAL=$?
@@ -22,14 +19,6 @@ echo "CHECK 1: Checking python version"
 python -c 'import sys ; assert sys.version_info.major == 3 and sys.version_info.minor >= 4, 'please install python 3.4 or 3.5 or 3.6 \(you have %s.%s\)' % \(sys.version_info.major, sys.version_info.minor\)'
 echo "Succes: correct version of python is installed"
 
-#check if pip is installed
-echo
-echo "CHECK 2: Checking if correct version of pip is installed"
-export succes="Succes: correct version of pip is installed"
-export failure="Fail: please install pip (please install at least pip version 9.0.1)"
-
-pip install "pip>=9.0.1"
-command_check
 
 #install external python modules
 echo
@@ -37,8 +26,11 @@ echo "INSTALL: Installing external python modules"
 export succes="Succes: external modules have been installed"
 export failure="Fail: please check external_modules.txt and try to install them"
 
-pip install -r external_modules.txt
-python$python_version -c "import newspaper; assert newspaper.__version__ == '0.1.9', 'incorrect version of newspaper module is installed: %s (should be 0.1.9' % newspaper.__version__"
+# installing external module newspaper
+python3 -m pip install newspaper3k==0.1.9
+
+# checking if newspaper module can imported
+python3 -c "import newspaper; assert newspaper.__version__ == '0.1.9', 'incorrect version of newspaper module is installed: %s (should be 0.1.9' % newspaper.__version__"
 command_check
 
 echo ""
