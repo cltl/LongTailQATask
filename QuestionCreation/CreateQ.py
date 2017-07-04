003065843addad051ed8f6ec5d6cdfdcf3cc69b2
MartenPostma <martenp@gmail.com>
2017-07-04 15:35:55 +0200

    trial data creation

--- QuestionCreation/CreateQ.py
+++ QuestionCreation/CreateQ.py
@@ -1,27 +1,38 @@
 import sys
 import pandas
-from datetime import datetime
-import json
-from collections import defaultdict
 
-from classes import Question
-import classes
 import look_up_utils
 import createq_utils
-from display_utils import display_question
+import pickle
+import argparse
 
 if __name__=="__main__":
-    
-    df = pandas.read_pickle('../EventRegistries/GunViolenceArchive/frames/all')
-    confusion_tuple = ('location', 'time')
-    event_types=['killing', 'injuring']
-    if len(sys.argv)>1:
-        subtask=int(sys.argv[1])
-    else:
-        subtask=2
 
-    print("Generating questions for subtask %d" % subtask)
+    parser = argparse.ArgumentParser(description='Create data for SemEval-2018 task 5')
+    parser.add_argument('-d', dest='path_gva_df', required=True, help='path to gva frame (../EventRegistries/GunViolenceArchive/frames/all)')
+    parser.add_argument('-e', dest='event_types', required=True, help='event types separated by underscore e.g. killing_injuring')
+    parser.add_argument('-s', dest='subtask', required=True, help='1 | 2 | 3')
+    parser.add_argument('-o', dest='output_folder', required=True, help='folder where output wsd will be stored')
+    args = parser.parse_args()
 
+    # load arguments
+    confusion_tuples = [('location', 'time'),
+                        ('participant', 'time'),
+                        ('location', 'participant')]
+    event_types = args.event_types.split('_')
+    subtask = int(args.subtask)
+    all_candidates = set()
+    df = pandas.read_pickle(args.path_gva_df)
+
+    output_path = '%s/%s---%s.bin' % (args.output_folder, args.subtask, args.event_types)
+
+    if subtask not in {1,2,3}:
+        print('invalid subtask: %s' % subtask)
+        sys.exit()
+
+    print(args.__dict__)
+
+    # set min and max number of incidents
     min_num_answer_incidents = 0
     max_num_answer_incidents = 99999
     count_participants=False
@@ -31,29 +42,30 @@ if __name__=="__main__":
         max_num_answer_incidents = 1
     elif subtask==2:
         min_num_answer_incidents = 2
-    elif subtask!=3:
-        print("Invalid subtask. Exiting now...")
-        sys.exit()
 
-    look_up, parameters2incident_uris = look_up_utils.create_look_up(df, 
-                                                                 discard_ambiguous_names=True,
-                                                                 allowed_incident_years={2017},
-                                                                 check_name_in_article=True)
+    # create questions
+    for confusion_tuple in confusion_tuples:
+        look_up, parameters2incident_uris = look_up_utils.create_look_up(df,
+                                                                     discard_ambiguous_names=True,
+                                                                     allowed_incident_years={2017},
+                                                                     check_name_in_article=True)
 
-    candidates=createq_utils.lookup_and_merge(look_up, 
-                                          parameters2incident_uris,
-                                          confusion_tuple,
-                                          min_num_answer_incidents,
-                                          max_num_answer_incidents,
-                                          subtask,
-                                          event_types,
-                                          df,
-                                          debug=False,
-                                          inspect_one=False,
-                                          set_attr_values=True) 
+        candidates=createq_utils.lookup_and_merge(look_up,
+                                                  parameters2incident_uris,
+                                                  confusion_tuple,
+                                                  min_num_answer_incidents,
+                                                  max_num_answer_incidents,
+                                                  subtask,
+                                                  event_types,
+                                                  df,
+                                                  debug=False,
+                                                  inspect_one=False,
+                                                  set_attr_values=False)
+        all_candidates.update(candidates)
 
-    print(len(candidates))
-    print({c.granularity for c in candidates})
+        print(confusion_tuple, len(candidates))
 
-    for c in candidates:
-        print(c.question(), c.answer)
+
+    # write to file
+    with open(output_path, 'wb') as outfile:
+        pickle.dump(all_candidates, outfile)
---
+++ QuestionCreation/create_task_data.py
@@ -0,0 +1,68 @@
+import argparse
+import subprocess
+import json
+from glob import glob
+import pickle
+import sys
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Create participant data for SemEval-2018 task 5')
+    parser.add_argument('-d', dest='input_folder', required=True, help='all .bin files in this folder will be considered')
+    parser.add_argument('-o', dest='output_folder', required=True, help='folder output will be stored')
+    args = parser.parse_args()
+
+    # bash commands
+    bash_commands = ['rm -rf %s' % args.output_folder,
+                     'mkdir -p %s/system_input' % args.output_folder,
+                     'mkdir -p %s/system_output' % args.output_folder,
+                     'cp ../Installing/setup.sh %s' % args.output_folder]
+
+    for bash_command in bash_commands:
+        output = subprocess.check_output(bash_command, shell=True)
+
+
+    # load all candidates
+    all_candidates = set()
+    for bin_path in glob(args.input_folder + '/*.bin'):
+        candidates = pickle.load(open(bin_path, 'rb'))
+        all_candidates.update(candidates)
+        print(len(all_candidates))
+
+
+
+    # convert trial data
+    questions = dict()
+    answers = dict()
+
+    maximum = 20
+    counter = 0
+    for candidate in candidates:
+
+        counter += 1
+        if counter == maximum:
+            break
+
+        dfs = [('gold', candidate.answer_df),
+               # ('confusion', candidate.confusion_df)
+               ]
+
+        output_path = '%s/system_input/%s.conll' % (args.output_folder, candidate.q_id)
+        candidate.to_conll_one_file_per_question(dfs, output_path)
+        one_question = candidate.question()
+
+        if len(candidate.all_doc_ids) != candidate.answer:
+            print('mismatch answer and incidents/documents')
+
+        if all([one_question is not None,
+                len(candidate.all_doc_ids) >= 2]):
+            questions[candidate.q_id] = one_question
+            answers[candidate.q_id] = {'numerical_answer': len(candidate.all_doc_ids),
+                                       'answer_docs': candidate.all_doc_ids}
+
+    question_out_path = '%s/questions.json' % args.output_folder
+    with open(question_out_path, 'w') as outfile:
+        outfile.write(json.dumps(questions, indent=4, sort_keys=True))
+
+    answers_out_path = '%s/answers.json' % args.output_folder
+    with open(answers_out_path, 'w') as outfile:
+        outfile.write(json.dumps(answers, indent=4, sort_keys=True))
