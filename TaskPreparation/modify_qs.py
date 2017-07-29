cdc077f07e5e1f5d40f4b00db856ab5df92e7a00
Filip Ilievski <filip.dbrsk@gmail.com>
2017-07-29 18:17:35 +0000

    upgrade modify qs

--- TaskPreparation/modify_qs.py
+++ TaskPreparation/modify_qs.py
@@ -8,9 +8,15 @@ for infile, outfile in zip(inputs,outputs):
         q_json=json.load(qj)
         new_json={}
         for q_id in q_json.keys():
-            new_json[q_id]=q_json[q_id]
+            new_json[q_id]={}
+            new_json[q_id]["subtask"]=q_json[q_id]["subtask"]
+            new_json[q_id]["verbose_question"]=q_json[q_id]["verbose_question"]
+            new_json[q_id]["event_type"]=q_json[q_id]["event_types"][0]
+            if len(q_json[q_id]["event_types"])!=1:
+                print(q_id)
             for prop in ['time', 'location', 'participant']:
-                if prop in new_json[q_id]:
-                    new_json[q_id][prop]={new_json[q_id][prop][1]: new_json[q_id][prop][0]}
+                if prop in q_json[q_id]:
+                    #new_json[q_id][prop]=q_json[q_id][prop]
+                    new_json[q_id][prop]={q_json[q_id][prop][1]: q_json[q_id][prop][0]}
         with open(outfile, 'w') as w:
             w.write(json.dumps(new_json, indent=4, sort_keys=True))
