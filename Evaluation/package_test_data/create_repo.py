b81b32e77bc34550786d7bf883febb556e1e8623
filipdbrsk <filip.dbrsk@gmail.com>
2018-02-09 14:20:15 +0100

    integers at end of verbose.conll

--- Evaluation/package_test_data/create_repo.py
+++ Evaluation/package_test_data/create_repo.py
@@ -9,19 +9,12 @@ verbose_path = '{out_dir}/verbose.conll'.format_map(locals())
 system_input_path = '{out_dir}/system_input.conll'.format_map(locals())
 gold_path = '{out_dir}/gold.conll'.format_map(locals())
 
-# move verbose file
-command = 'cp {input_path} {out_dir}/verbose.conll'.format_map(locals())
-commands = [command]
-for command in commands:
-    check_output = subprocess.check_output(command, shell=True)
-    print(check_output)
-
-
 # create system input and gold
-with open(verbose_path, 'r') as infile:
+with open(input_path, 'r') as infile:
 
     system_file = open(system_input_path, 'w')
     gold_file = open(gold_path, 'w')
+    verbose_file = open(verbose_path, 'w')
 
     for line in infile:
 
@@ -33,18 +26,21 @@ with open(verbose_path, 'r') as infile:
             token_id, token, discourse, integer, verbose = line.strip().split('\t')
 
             for label, file in [('system', system_file),
-                                ('gold', gold_file)]:
+                                ('gold', gold_file),
+                                ('verbose', verbose_file)]:
 
                 if label == 'system':
                     one_row = [token_id, token, discourse, '-']
                 elif label == 'gold':
                     one_row = [token_id, token, discourse, integer]
-
+                elif label == 'verbose':
+                    one_row = [token_id, token, discourse, verbose, integer]
 
                 file.write('\t'.join(one_row) + '\n')
 
     system_file.close()
     gold_file.close()
+    verbose_file.close()
 
 
 
